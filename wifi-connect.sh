bb493af8efafc862576017bf006d94ef0fc270cf
Ce Gao <gaocegege@hotmail.com>
2016-03-12 20:37:24 +0800

    add the s to use ascii key

--- wifi-connect.sh
+++ wifi-connect.sh
@@ -2,9 +2,11 @@
 
 USAGE="./wifi-connect.sh <ESSID> <PWD>"
 
+echo "$#"
+
 if [[ "$#" -ne 2 ]]; then
 	echo ${USAGE}
 	exit 0
 fi
 
-sudo iwconfig wlan0 essid $1 key $2
+sudo iwconfig wlan0 essid $1 key "s:$2"
